bcd5a477c7acb751e0796826b43b602c4504870c
bkantwi <bernardantwi2010@gmail.com>
2024-01-23 02:38:51 +0000

    added buttons

--- index.py
+++ index.py
@@ -21,6 +21,19 @@ image3=PhotoImage(file="recording.png")
 Label(root,image=image3,bd=0).pack(pady=30)
 
 #buttons
+start=Button(root,text="Start",font="arial 22",bd=0)
+start.place(x=140,y=250)
 
+image4=PhotoImage(file="pause.png")
+pause=Button(root,image=image4,bd=0,bg="#fff")
+pause.place(x=50,y=450)
+
+image5=PhotoImage(file="play.png")
+play=Button(root,image=image5,bd=0,bg="#fff")
+play.place(x=50,y=450)
+
+image6=PhotoImage(file="stop.png")
+stop=Button(root,image=image6,bd=0,bg="#fff")
+stop.place(x=50,y=450)
 
 root.mainloop()
